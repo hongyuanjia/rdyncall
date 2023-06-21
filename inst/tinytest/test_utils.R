8f4dddf017f81ae0b3f1d7a0c39152a964acf7e1
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-06-21 20:59:39 +0800

    test(utils): remove duplicated test cases

--- inst/tinytest/test_utils.R
+++ inst/tinytest/test_utils.R
@@ -1,6 +1,3 @@
 expect_false(is.nullptr(NULL))
 expect_equal(class(as.externalptr(1)), "externalptr")
 expect_true(is.externalptr(offset_ptr(1, 1L)))
-
-expect_equal(class(as.floatraw(1)), "floatraw")
-expect_equal(floatraw2numeric(as.floatraw(1:2)), 1:2)
