a657846ff613febbad6e56bc389d6734c154b5b2
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-06-23 15:13:32 +0800

    test(dynload): use full path of C math library

--- inst/tinytest/test_dynload.R
+++ inst/tinytest/test_dynload.R
@@ -1,4 +1,6 @@
-expect_true(is.externalptr(libc <- dynload(c("msvcrt", "c", "c.so.6"))))
-expect_true(is.externalptr(c_sqrt <- dynsym(libc, "sqrt")))
+expect_true(is.externalptr(
+    libm <- dynload(c("/lib/x86_64-linux-gnu/libm.so.6", "msvcrt", "m", "m.so.6"))
+))
+expect_true(is.externalptr(c_sqrt <- dynsym(libm, "sqrt")))
 expect_equal(dyncall(c_sqrt, "d)d", 144), 12)
-expect_null(dynunload(libc))
+expect_null(dynunload(libm))
