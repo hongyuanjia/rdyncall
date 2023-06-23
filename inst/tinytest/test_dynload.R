30d02ad2aaf66ad28bf2372d1a8cc9888058a38a
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-06-23 16:54:05 +0800

    test(dynload): use `dynfind()`

--- inst/tinytest/test_dynload.R
+++ inst/tinytest/test_dynload.R
@@ -1,6 +1,4 @@
-expect_true(is.externalptr(
-    libm <- dynload(c("/lib/x86_64-linux-gnu/libm.so.6", "msvcrt", "m", "m.so.6"))
-))
+expect_true(is.externalptr(libm <- dynfind(c("msvcrt", "m", "m.so.6"))))
 expect_true(is.externalptr(c_sqrt <- dynsym(libm, "sqrt")))
 expect_equal(dyncall(c_sqrt, "d)d", 144), 12)
 expect_null(dynunload(libm))
