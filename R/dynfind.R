c662db09e2d72e9d874db61af28d20d8b616dafc
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-06-25 22:56:59 +0800

    fix(dynfind): try full library path under macOS

--- R/dynfind.R
+++ R/dynfind.R
@@ -13,7 +13,21 @@ dynfind1 <- if (.Platform$OS.type == "windows") {
             if (!is.null(handle)) {
                 return(handle)
             }
-            dynload(paste("lib", name, ".dylib", sep = ""), ...)
+            handle <- dynload(paste("lib", name, ".dylib", sep = ""), ...)
+            if (!is.null(handle)) {
+                return(handle)
+            }
+            # ref: https://developer.apple.com/forums/thread/692383
+            # ref: https://developer.apple.com/forums/thread/655588
+            #
+            # Start from macOS 11.1, a dynamic linker shared cache is used
+            # and there are no common .dylib files under '/usr/lib'.
+            #
+            # All the commonly-used dynamic libraries are pre-linked together
+            # into a single shared file called 'libSystem.dylib'.
+            #
+            # Here have to try the full path
+            dynload(paste("/usr/lib/lib", name, ".dylib", sep = ""), ...)
         }
     } else {
         function(name, ...) {
