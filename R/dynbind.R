73d822cfdfe4c96e2e2e2a310bbd0815aece6133
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2023-06-27 15:00:04 +0800

    doc: convert dynbind Rd to roxygen2 comments

--- R/dynbind.R
+++ R/dynbind.R
@@ -1,13 +1,113 @@
-# Package: rdyncall
-# File: R/dynbind.R
-# Description: single-entry front-end to dynamic binding of library functions
-
+#' Binding C library functions via thin call wrappers
+#'
+#' @description
+#' Function to bind several foreign functions of a C library via installation of
+#' thin R call wrappers.
+#'
+#' @details
+#' `dynbind()` makes a set of C functions available to R through installation of
+#' thin call wrappers. The set of functions, including the symbolic name and
+#' function type, is specified by `signature`; a character string that encodes a
+#' library signature:
+#'
+#' The **library signature** is a compact plain-text format to specify a
+#' set of function bindings.
+#' It consists of function names and corresponding [call signature].
+#' Function bindings are separated by ";" (semicolon);
+#' white spaces (including tab and new line) are allowed before and after
+#' semicolon.
+#'
+#' ```
+#' function-name ( call-signature ; ...
+#' ```
+#'
+#' Here is an example that specifies three function bindings to the OpenGL
+#' library:
+#'
+#' ```
+#' `"glAccum(If)v ; glClear(I)v ; glClearColor(ffff)v ;"`
+#' ```
+#'
+#' Symbolic names are resolved using the library specified by `libnames`
+#' using [dynfind()] for loading.
+#' For each function, a thin call wrapper function is created using the
+#' following template:
+#'
+#' ```
+#' function(...) .dyncall.<MODE> ( <TARGET>, <SIGNATURE>, ... )
+#' ```
+#'
+#' `<MODE>` is replaced by `callmode` argument, see [dyncall()] for details on
+#' calling conventions.
+#' `<TARGET>` is replaced by the external pointer, resolved by the
+#' `function-name`.
+#' `<SIGNATURE>` is replaced by the call signature string contained in
+#' `signature`.
+#'
+#' The call wrapper is installed in the environment given by `envir`.
+#' The assignment name is obtained from the function signature.
+#' If `pattern` and `replace` is given, a text replacement is applied to the
+#' name before assignment, useful for basic C name space mangling such as
+#' exchanging the prefix.
+#'
+#' As a special case, [dynbind()] supports binding of pointer-to-function
+#' variables, indicated by setting `funcptr` to `TRUE`, in which case
+#' `<TARGET>` is replaced with the expression `unpack(<TARGET>,"p",0)` in order
+#' to dereference `<TARGET>` as a pointer-to-function variable at call-time.
+#'
+#' @param libnames vector of character strings giving short library names of the
+#'        shared library to be loaded. See [dynfind()] for details.
+#'        `
+#' @param signature character string specifying the *library signature* that
+#'        determines the set of foreign function names and types. See details.
+#'
+#' @param envir the environment to use for installation of call wrappers.
+#'
+#' @param callmode character string specifying the calling convention, see
+#'        details.
+#'
+#' @param pattern `NULL` or regular expression character string applied to
+#'        symbolic names.
+#'
+#' @param replace `NULL` or replacement character string applied to `pattern`
+#'        part of symbolic names.
+#'
+#' @param funcptr logical, that indicates whether foreign objects refer to
+#'        functions (`FALSE`, default) or to function pointer variables
+#'        (`TRUE` rarely needed).
+#'
+#' @return
+#' The function returns a list with two fields:
+#'
+#' \item{libhandle}{External pointer returned by [dynload()].}
+#' \item{unresolved.symbols}{vector of character strings, the names of
+#' unresolved symbols.}
+#'
+#' As a side effect, for each wrapper, [dynbind()] assigns the
+#' `function-name` to  the corresponding call wrapper function in the
+#' environment given by `envir`.
+#'
+#' If no shared library is found, an error is reported.
+#'
+#' @examples
+#' \donttest{
+#' info <- dynbind("R", "R_ShowMessage(Z)v; R_rsort(pi)v;")
+#' R_ShowMessage("hello")
+#' }
+#'
+#' @seealso
+#' [dyncall()] for details on call signatures and calling conventions,
+#' [dynfind()] for details on short library names,
+#' [unpack()] for details on reading low-level memory (e.g. dereferencing of
+#' (function) pointer variables).
+#'
+#' @keywords programming interface
 # TODO: use named character vector for signatures?
 dynbind <- function(libnames, signature, envir = parent.frame(), callmode = "default", pattern = NULL, replace = NULL, funcptr = FALSE) {
     # load shared library
     libh <- dynfind(libnames)
     if (is.null(libh)) {
-        cat("dynbind error: Unable to find shared library '", libnames[[1]], "'.\n", sep = "")
+        cat("dynbind error: Unable to find shared library '", libnames1, "'.\n", sep = "")
         cat("For details how to install dynport shared libs, type: ?'rdyncall-demos' might help.\n")
         cat("If there is no information about your OS, consult the projects page how to build and install the shared library for your operating-system.\n")
         cat("Make sure the shared library can be found at the default system places or adjust environment variables (e.g. %PATH% or $LD_LIBRARY_PATH).\n")
