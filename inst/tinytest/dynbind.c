80d6046e250afb9796485f2f7cbb5c6100556238
Hongyuan Jia <hongyuanjia@cqust.edu.cn>
2026-06-24 02:42:30 +0800

    feat: add variadic dyncall support (#35)

--- inst/tinytest/dynbind.c
+++ inst/tinytest/dynbind.c
@@ -1,4 +1,32 @@
+#include <stdarg.h>
+
 int rdyncall_dynbind_add(int x, int y)
 {
+    return x + y;
+}
+
+int rdyncall_dynbind_sum_variadic(int x, ...)
+{
+    va_list args;
+    int y;
+    int z;
+
+    va_start(args, x);
+    y = va_arg(args, int);
+    z = va_arg(args, int);
+    va_end(args);
+
+    return x + y + z;
+}
+
+double rdyncall_dynbind_sum_variadic_double(double x, ...)
+{
+    va_list args;
+    double y;
+
+    va_start(args, x);
+    y = va_arg(args, double);
+    va_end(args);
+
     return x + y;
 }
